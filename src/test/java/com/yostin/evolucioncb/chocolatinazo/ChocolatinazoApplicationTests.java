085251ee7a0d7da4bd18e0585a3aadf3f771c294
Yostin <ysmaprogramming@gmail.com>
2026-03-21 22:38:36 -0500

    initial commit

---
+++ src/test/java/com/yostin/evolucioncb/chocolatinazo/ChocolatinazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yostin.evolucioncb.chocolatinazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolatinazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

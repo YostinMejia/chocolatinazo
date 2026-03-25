20edf5f3247fcca6b2c027dce7a390dedaeb2a05
Yostin <ysmaprogramming@gmail.com>
2026-03-24 23:20:38 -0500

    feat(game): implement audit finished games controller for Auditors

--- src/main/java/com/yostin/evolucioncb/chocolatinazo/domain/finishedgame/repositories/FinishedGameRepository.java
+++ src/main/java/com/yostin/evolucioncb/chocolatinazo/domain/finishedgame/repositories/FinishedGameRepository.java
@@ -2,7 +2,10 @@ package com.yostin.evolucioncb.chocolatinazo.domain.finishedgame.repositories;
 
 import com.yostin.evolucioncb.chocolatinazo.domain.finishedgame.models.FinishedGame;
 
+import java.util.List;
+
 public interface FinishedGameRepository {
     FinishedGame save(FinishedGame finishedGame);
+    List<FinishedGame> findAll();
 
 }
